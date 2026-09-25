33a99d823d2856336a42158d7ed3c06470d17c33
thesolelane <thesolelane@gmail.com>
2026-09-24 21:28:51 -0400

    fix(guest): create guest id before first API call

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,5 +2,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import "./wallet-button.css";
+import { getGuestId } from "./lib/guest-id";
+
+getGuestId();
 
 createRoot(document.getElementById("root")!).render(<App />);
